2cee52961f4c4a9b9dd83f6cb1740fc44d2b7ccf
Zeudfish <85560105+Zeudfish@users.noreply.github.com>
2023-08-21 10:58:03 +0800

    Update mydataset_image.py

---
+++ mydataset_image.py
@@ -0,0 +1,55 @@
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+import json
+import numpy as np
+import os
+import cv2
+
+
+
+class MyDataSet(Dataset):
+
+    
+    CLASSES = []
+    def __init__(
+            self, 
+            images_dir, 
+            masks_dir, 
+            classes=None, 
+            augmentation=None, 
+            preprocessing=None,
+    ):
+        self.ids = os.listdir(images_dir)
+        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
+        self.masks_fps = [os.path.join(masks_dir, image_id) for image_id in self.ids]
+        
+        # convert str names to class values on masks
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+    
+    def __getitem__(self, i):
+        
+        # read data
+        image = cv2.imread(self.images_fps[i])
+        mask = cv2.imread(self.masks_fps[i], 0)
+        
+
+        masks = [(mask == v) for v in self.class_values]
+        mask = np.stack(masks, axis=-1).astype('float')
+        
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+            
+        return image, mask
+        
+    def __len__(self):
+        return len(self.ids)
